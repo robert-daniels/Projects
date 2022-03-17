78da2d403e2320092530bf89565e789cb8c9c95a
Robert Daniels <robertdanielsvc@gmail.com>
2022-03-17 14:17:55 -0500

    add udemy algo folder

--- CS 205/throwaway.java
+++ CS 205/throwaway.java
@@ -1,12 +1,22 @@
-
+import java.util.*;
+import java.util.*;
 
 public class throwaway {
 
     public static void main(String[] args){
 
-        int myInt = -12;
+        int myInt = 12;
+        String myString = "hello";
 
-        System.out.printf("Value:%+d", myInt);
+        System.out.printf("%08d\n", myInt);
+
+        String mySentence = "Hello my name is Robert Daniels";
+
+        Scanner iSS  = new Scanner(mySentence);
+
+        String firstWord = iSS.next();
+
+        System.out.println(firstWord);
     }
     
 }
---
+++ Udemy_Algo/Arrays/BubbleSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class BubbleSort {
+    
+    public static void main(String[] args) {
+        /**
+         * Bubble sort degrads rapidly, but warm up.
+         * Compare array[i] to array[i+1], if greater, swap values 
+         * The larger values "bubble up."
+         * 
+         * Stable sort unless comparison is >= 
+         * 
+         */
+
+         int testArray[] = {3, 1, 4, 6, 7, -9};
+
+         int swapHold;
+         int sortedIndex  = testArray.length;
+
+         while (sortedIndex != 0){
+             for (int i = 0; i < sortedIndex - 1; ++i){  // not clean
+                 if (testArray[i] > testArray[i+1]){
+                     swapHold = testArray[i];
+                     testArray[i] = testArray[i+1];
+                     testArray[i+1] = swapHold;
+                 }
+             }
+             --sortedIndex;
+         }
+
+         System.out.println(Arrays.toString(testArray));
+
+    }
+}
