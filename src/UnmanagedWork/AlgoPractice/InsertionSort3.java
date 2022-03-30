ff6bf6a9b1d500393d1c45daff746ba0a7d6dbd5
Robert Daniels <robertdanielsvc@gmail.com>
2022-03-30 16:52:11 -0500

    more algo practice

---
+++ src/UnmanagedWork/AlgoPractice/InsertionSort3.java
@@ -0,0 +1,27 @@
+package UnmanagedWork.AlgoPractice;
+
+import java.util.*;
+
+/**
+ * Again, practice only. See original for docstring
+ */
+
+public class InsertionSort3 {
+   
+    public static void main(String[] args){
+
+        SwapTwo swapper = new SwapTwo();
+        int[] testArr = {5, 4, 3, 2, 1};
+        int j;
+
+        for (int i = 1; i < testArr.length; ++i){
+            j = i;
+            while(j > 0 && testArr[j] < testArr[j - 1]){
+                swapper.swapTwo(testArr, j, j - 1);
+                --j;
+            }
+        }
+
+        System.out.println(Arrays.toString(testArr));
+    }
+}
--- src/UnmanagedWork/AlgoPractice/SelectionSort3.java
+++ src/UnmanagedWork/AlgoPractice/SelectionSort3.java
@@ -2,24 +2,22 @@ package UnmanagedWork.AlgoPractice;
 
 import java.util.*;
 
-
 /**
- * Next day practice
+ * More practice
+ * 
  */
+public class SelectionSort3 {
 
-public class SelectionSort2 {
-    
     public static void main(String[] args){
-
-        SwapTwo swapper = new SwapTwo();
-
+        
         int[] testArr = {5, 4, 3, 2, 1};
+        SwapTwo swapper = new SwapTwo();
         int minIndex = 0;
 
         for (int i = 0; i < testArr.length; ++i){
             minIndex = i;
             for (int j = i; j < testArr.length; ++j){
-                if ( testArr[j] < testArr[minIndex]){
+                if (testArr[j] < testArr[minIndex]){
                     minIndex = j;
                 }
             }
@@ -28,6 +26,5 @@ public class SelectionSort2 {
         }
 
         System.out.println(Arrays.toString(testArr));
-
-    } 
+    }
 }
