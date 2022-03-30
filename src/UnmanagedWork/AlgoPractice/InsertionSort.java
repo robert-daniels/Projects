f8faec03946b3cf3b4a08b19de3b3f1da33e4650
Robert Daniels <robertdanielsvc@gmail.com>
2022-03-30 14:38:43 -0500

    add in insertionsorts and another binary search

---
+++ src/UnmanagedWork/AlgoPractice/InsertionSort.java
@@ -0,0 +1,33 @@
+package UnmanagedWork.AlgoPractice;
+
+import java.util.*;
+
+/**
+ * Insertion Sort: goes through each element in the unsorted portion of the array and inserts
+ * it into its correct position in the sorted portion of the array. 
+ * 
+ * #TODO: O(N^2) 
+ */
+
+public class InsertionSort {
+    
+    public static void main(String[] args){
+
+        int[] testArr = {5, 4, 3, 2, 1};
+        SwapTwo swapper = new SwapTwo();
+        int j;
+
+        for (int i = 1; i < testArr.length; ++i){
+           j = i;
+           while (j > 0 && testArr[j] < testArr[j - 1]){
+               swapper.swapTwo(testArr, j, j-1);
+               --j; // VERY IMPORTANT! Protip: Don't forget this step.
+           }
+           
+        }
+
+        System.out.println(Arrays.toString(testArr));
+
+    }
+
+}
