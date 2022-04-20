18fa813f212ff6e7a07c210cddb9436ca32568b4
Robert Daniels <robertdanielsvc@gmail.com>
2022-04-20 11:57:38 -0500

    add tvm function

--- JS/homework/midterm/midterm.js
+++ JS/homework/midterm/midterm.js
@@ -28,5 +28,63 @@ function calcSelector() {
 
 }
 
+// runs the basic arithmetic functions requested under basic math
+
+function runMath() {
+    // similar structure to seed file provided. No data sanitation / error checking for nonsensical user inputs.
+
+    let operationChoice = document.getElementById("mathOperator").value;
+
+    let localNum1 = +document.getElementById("num1").value;
+    let localNum2 = +document.getElementById("num2").value;
+
+    let result = '';
+
+    // REQUIREMENT 1: evaluate user data AND REQUIREMENT 2, =, ==, ===
+    if (operationChoice === "add"){
+        result = localNum1 + localNum2;
+    }
+    else if (operationChoice === "subtract"){
+        result = localNum1 - localNum2;
+    }
+    else if (operationChoice === "multiply"){
+        result = localNum1 * localNum2;
+    }
+    else if (operationChoice === "divide"){
+        result = localNum1 / localNum2;
+    }
+    else if (operationChoice === "exponent"){
+        result = localNum1 ** localNum2;
+    }
+
+    // updates p tag at the end of mathDiv with result
+    //pulls to 2 decimal points to correct for machine precision
+    document.getElementById("mathAnswer").textContent = "$" + result.toFixed(2);
+
+    
+}
+
+// calculates future value of money. REQUIREMENT 3: PEMDAS
+
+function tvmCalc(form){
+    let presentValue = form.presentValue.value;
+    let interestRate = form.interestRate.value;
+    let periodsPerYear = form.periodsPerYear.value;
+    let years = form.years.value;
+
+    let futureValue = presentValue * ((1 + (interestRate / periodsPerYear))**(periodsPerYear * years))
+    futureValue = futureValue.toFixed(2);
+
+    document.getElementById("tvmAnswer").textContent = "$" + futureValue;
+
+}
+
+
+// onclick assignments
+
+// choose method to run
 document.getElementById("startCalc").onclick = calcSelector;
+
+// calculate basic math
+document.getElementById("mathCalculate").onclick = runMath;
 
