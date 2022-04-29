7366a89fc2d7eeda767f3f88eca023fb336da213
Robert Daniels <robertdanielsvc@gmail.com>
2022-04-29 14:29:25 -0500

    adding array methods

--- JS/homework/final/final.js
+++ JS/homework/final/final.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// =============================Establish global variables=============================
+// =============================Establish variables=============================
 let name = prompt("Hello! What is your name?", "John Smith"); // window.prompt(), let 
 //alert(`${name}, let's play some blackjack!`) // window.alert() //TODO: uncomment
 
@@ -11,7 +11,7 @@ let instructions = "the rules of this game are simple: Answer questions about Ja
 let userPoints = 0;
 let dealerHand = 0;
 let currentTopic;
-let randomArrayChoice = Math.floor(Math.random() * 3);
+let randomArrayChoice = Math.floor(Math.random() * 3); // assumes three questions per category. brittle, could instead use the array.length
 let gameRunning = true;
 
 
@@ -19,7 +19,6 @@ let gameRunning = true;
 
 // document.getElementById("something1").textContent
 document.getElementById("instructions").textContent = `${name}, ` + instructions
-
 
 
 // =============================onclick Assignments =============================
@@ -42,7 +41,7 @@ Removes the initial instructions and loads the game board.
 function loadGameBoard() {  // create several custom functions to process your game
     userPoints = 0
 
-    document.getElementById("questionSocket").classList.add("deactivatedDiv");
+    document.getElementById("questionSocket").className = "deactivatedDiv";
     document.getElementById("title").textContent = "Let's Play A Game:"
     // document.getElementById("something5").classList
     document.getElementById("onboardingDiv").classList.add("deactivatedDiv");
@@ -124,8 +123,15 @@ function askQuestion() {
         case "loops":
             loopQuizzer.askQuestion(randomArrayChoice);
             currentTopic = topicChoice;
-
-
+            break;
+        case "dataTypes":
+            dataTypeQuizzer.askQuestion(randomArrayChoice);
+            currentTopic = topicChoice;
+            break;
+        case "arrays":
+            arrayQuizzer.askQuestion(randomArrayChoice); 
+            currentTopic = topicChoice;
+            break;
     }
 
 }
@@ -138,24 +144,33 @@ function gradeAnswer() {
     console.log("gradeAnswer ran");
     let validated = false;
     document.getElementById("answerResult").className = "activatedDiv";
+    let correctAnswer;
+    let userQuizAnswer = document.getElementById("userAnswer").value;
 
     switch (currentTopic){
         case ("loops"):
-            let correctAnswer = loopQuestions[randomArrayChoice].answer;
-            let userQuizAnswer = document.getElementById("userAnswer").value;
-            if (userQuizAnswer == (correctAnswer)) {  // if ... else
-                validated = true;
-                respondToAnswer(validated, correctAnswer); 
-            }
-            else {
-                console.log("false");
-                respondToAnswer(validated, correctAnswer);
-            }
+            correctAnswer = loopQuestions[randomArrayChoice].answer;
+            break;
+        case ("dataTypes") :
+            correctAnswer = dataTypeQuestions[randomArrayChoice].answer;
+            break;
+        case ("arrays") :
+            correctAnswer = arrayQuestions[randomArrayChoice].answer;
+            break;
+        case ("comparisons") :
+            correctAnswer = "method stub" //TODO:
+            break;
+        default:
+            console.log("something went wrong in final.js/gradeAnswer()");
+        }
 
-           
-            console.log(loopQuestions);
-
-
+    if (userQuizAnswer == (correctAnswer)) {  // if ... else
+        validated = true;
+        respondToAnswer(validated, correctAnswer); 
+    }
+    else {
+        console.log("false");
+        respondToAnswer(validated, correctAnswer);
     }
 }
 
@@ -165,7 +180,7 @@ Output to user if the answer was right or wrong. Calls addPoints()
 
 function respondToAnswer(validated, correctAnswer) {
     let answerResult = document.getElementById("answerResult")
-    document.getElementById("answerSocket").classList.add("deactivatedDiv");
+    document.getElementById("answerSocket").className = "deactivatedDiv";
 
     if (validated) {
         answerResult.textContent = "Correct!"
@@ -229,12 +244,73 @@ const loopQuizzer = {
 
 // =============================Data Types and Related Functions=============================
 
+const dataTypesQ1 = {
+    question: "Which of the following are NOT a pimitive data type in JavaScript: string, number, bigint, smallint, boolean, symbol, null, undefined",
+    answer: "smallint",
+}
+
+const dataTypesQ2 = { // +   -   *   /   %   **
+    question: "What will Math.floor((4.0 + 6 - (9 / 3)) ** (10 % 3)) evaluate to?",
+    answer: "7",
+}
+
+const dataTypesQ3 = {
+    question: "Assuming that you have num1 = 2 and num2 = 3, cast as strings, what will the result of num3 = num1 + num2 be?",
+    answer: "23",
+    // Differentiate the use of + to add versus concatenate.
+    explanation: "Using + in this context between two strings will concatenate the two strings, not add the two numbers. To add two numbers, at least one number should be cast to a number type using the unary operator + (example: +num1 + num2)" //TODO: add in this portion
+}
+
+let dataTypeQuestions = [dataTypesQ1, dataTypesQ2, dataTypesQ3];
+
+
+const dataTypeQuizzer = {
+    askQuestion : function (arrayChoice){
+        document.getElementById("questionSocket").className = ("activatedDiv");
+        document.getElementById("questionSocket").textContent = dataTypeQuestions[arrayChoice].question;
+        document.getElementById("answerSocket").className = "activatedDiv"
+    }
+}
 
 // =============================Arrays and Related Functions=============================
 
+const arrayQ1 = {
+    question: "Given let arr1 = ['football', 'soccer', 'swimming', 'tennis'], how would you access the swimming element?",
+    answer: "arr1[2]",
+}
 
-// =============================DOM and Related Functions=============================
+const arrayQ2 = { // for
+    question: "Suppose you wanted to iterate through each item in array, arr1. What keyword would replace the 'x' in this code? :  x (let key in arr1){ ...code block}",
+    answer: "for",
+}
 
+const arrayQ3 = {
+    question: "Given array arr1 = ['I', 'like', 'JavaScript', 'tons'], what command should be run to delete both 'like' and 'JavaScript'",
+    answer: "arr1.splice(1,2)",
+}
+
+
+let arrayQuestions = [arrayQ1, arrayQ2, arrayQ3];
+
+const arrayQuizzer = {
+    askQuestion : function (arrayChoice){
+        document.getElementById("questionSocket").className = ("activatedDiv");
+        document.getElementById("questionSocket").textContent = arrayQuestions[arrayChoice].question;
+        document.getElementById("answerSocket").className = "activatedDiv"
+    }
+}
+
+// =============================Comparisons and Related Functions=============================
+
+const comparisonsQ1 = {
+    question: "What will [(6 < 7) && (10 > 5) || (2 == '2')] return?", // >, <, &&, ||, == 
+    answer: "true",
+}
+
+const comparisonsQ2 = { // ===, !=, >=, <=
+    question: "What will [(6 != '6') && ('a' === 'A') || (7 >= 2) || (6 <= 8)] return?",
+    answer: "true",
+}
 
 
 // =============================Items Remaining to Code=============================
@@ -243,23 +319,18 @@ const loopQuizzer = {
 // document.querySelectorAll(".something6")
 
 // document.querySelectorAll(".something6")
-// for
 
 
 
-
-
-// addEventListener() to trigger all functions OR you can also use individual JavaScript event properties such as .onclick and assign a function to that property
 // use JavaScript to modify images
 
-// include ALL of the following operations, conditional operators, and logical operators
-// +   -   *   /   %   **
-// Differentiate the use of + to add versus concatenate.
-// ==   +=
-// ===   !=   !==
+
+
+//   +=
+// !==
 // ++    --   
-// >   <   >=   <=
-// &&   ||
+// 
+
 // Do not allow the user to divide by 0.
 
 // at least 2 images
