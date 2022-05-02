c06d5f0be105ca49848a7a07a452ace4edf484de
Robert Daniels <robertdanielsvc@gmail.com>
2022-05-02 09:19:10 -0500

    change to object literals JS final

--- JS/homework/final/final.js
+++ JS/homework/final/final.js
@@ -11,7 +11,7 @@ alert(`${name}, let's play!`) // window.alert()
 
 const winThreshold = 21; // const
 
-let instructions = "the rules of this game are simple: Answer questions about JavaScript to push your point value as close to 21 as possible. Aces will be either 1 or 10, the comptuer will decide. You win if you get closer to 21 than the dealer without going over. If you go over, you lose. Think you can do it?"
+let instructions = "the rules of this game are simple: Answer questions about JavaScript to push your point value as close to 21 as possible. You win if you get closer to 21 than the dealer without going over. If you go over, you lose. Think you can do it?"
 
 let userPoints;
 let dealerHand;
@@ -22,208 +22,182 @@ let gameRunning = true;
 
 // =============================Introduce the Game=============================
 
-// document.getElementById("something1").textContent
+// document.getElementById("something1").textContent  , show concatenate
 document.getElementById("instructions").textContent = `${name}, ` + instructions;
-
-
-// =============================onclick Assignments=============================
-// onclick assignment
-
-document.getElementById("userAgreeToStart").onclick = loadGameBoard;
-document.getElementById("askQuestion").onclick = askQuestion;
-document.getElementById('userQuizAnswer').onclick = gradeAnswer;
-document.getElementById('endGame').onclick = determineWin;
-document.getElementById("tryAgain").onclick = loadGameBoard;
-
 
 
 // =============================Backend Functions=============================
 
 `
-Removes the initial instructions and loads / resets the game board.
+Backend functions to run the game. Wrapped into an object literal Dealer because I wanted to practice
 `
+const Dealer = {
 
-function loadGameBoard() {  // create several custom functions to process your game
-    userPoints = 0;
-    document.getElementById("categorySelector").className = "activatedDiv";
+    loadGameBoard : function() {
+        userPoints = 0;
 
-    document.getElementById("questionSocket").className = "deactivatedDiv";
-    document.getElementById("title").textContent = "Let's Play!"
-    
-    // document.getElementById("something5").classList
-    document.getElementById("onboardingDiv").classList.add("deactivatedDiv");
-    
-    // document.getElementById("something4").className
-    document.getElementById("gameBoardDiv").className = "activatedDiv";
-    document.getElementById("answerResult").className = "deactivatedDiv";
-  
-    setDealerHand();
+        // display appropriate sections
+        document.getElementById("categorySelector").className = "activatedDiv";
+        document.getElementById("questionSocket").className = "deactivatedDiv";
+        document.getElementById("quizzer").className = "activatedDiv";
+        document.getElementById("pointCounter").className = "deactivatedDiv";
 
-    document.getElementById("pointCounter").textContent = `${name}, your current card count is ${userPoints}`;
-}
+        document.getElementById("title").textContent = "Let's Play!"
+        // document.getElementById("something5").classList
+        document.getElementById("onboardingDiv").classList.add("deactivatedDiv");
+        // document.getElementById("something4").className
+        document.getElementById("gameBoardDiv").className = "activatedDiv";
+        document.getElementById("answerResult").className = "deactivatedDiv";
+        Dealer.setDealerHand();
+        document.getElementById("pointCounter").textContent = `${name}, your current card count is ${userPoints}`;
+    },
 
-`
-Draw a dealer's hand. Determines the initial dealer value
-`
+    setDealerHand : function() {
+        dealerHand = 0
 
-function setDealerHand() {
-    dealerHand = 0
-
-    while (dealerHand < 11) {  // while
+        while (dealerHand < 11) {  // while
         dealerHand = dealerHand + Math.floor((Math.random() * 11) + 1);
         console.log(dealerHand) // console.log() to follow and debug your code -- leave these in the js file. 
-    }
-}
-
-`
-Determines if user or dealer wins and displays such. Regardless of win/lose, enables user to play again.
-`
-
-function determineWin() {
-    console.log("determineWin ran");
-    var titleElement = document.getElementById("tryAgainDiv").value
-    
-
-    if (userPoints > dealerHand && userPoints < 22) {
-        document.getElementById("title").textContent = `Your points: ${userPoints}. Dealer: ${dealerHand}...You win!`
-        
-    }
-    else if (userPoints > 21) {
-        document.getElementById("title").textContent = `Your points: ${userPoints}. You've gone over 21. You lose...`
-    }
-    else if (userPoints < dealerHand){
-        document.getElementById("title").textContent = `Your points: ${userPoints} Dealer: ${dealerHand}...Dealer wins!`
-    }
-    
-    
-    if (titleElement) {
-        title.append(document.getElementById("tryAgainDiv"))
-    }
-    
-}
-
-`
-Pulls a question from the relevant topic quiz bank
-`
-
-function askQuestion() {
-    console.log("askQuestion() ran")
-    
-    // document.getElementById("something2").value
-    let topicChoice = document.getElementById("categorySelector").value; 
-    currentTopic = topicChoice;
-    
-    
-    // clear the old responses
-    document.getElementById("answerResult").textContent = "";
-    document.getElementById("userAnswer").value = "";
-
-    randomArrayChoice = Math.floor(Math.random() * 3);
-    
-    // activate the answer template and append to the socket
-    document.getElementById("answerTemplate").className = "activatedDiv";
-    answerSocket.append(document.getElementById('answerTemplate'));
-    
-
-    switch (topicChoice){  // switch
-
-        case "loops":
-            loopQuizzer.askQuestion(randomArrayChoice);
-            break;
-        case "dataTypes":
-            dataTypeQuizzer.askQuestion(randomArrayChoice);
-            break;
-        case "arrays":
-            arrayQuizzer.askQuestion(randomArrayChoice); 
-            break;
-        case "comparisons":
-            comparisonsQuizzer.askQuestion(randomArrayChoice);
-            break;
-        default:
-            console.log("Something went wrong in final.js/askQuestion()");
-    }
-
-}
-
-`
-Validates the user input to see if correct based off topic chosen. If validated, add points to user score
-`
-
-function gradeAnswer() {
-    console.log("gradeAnswer ran");
-    let validated = false;
-    document.getElementById("answerResult").className = "activatedDiv";
-    let correctAnswer;
-    let userQuizAnswer = document.getElementById("userAnswer").value;
-
-    switch (currentTopic){
-        case ("loops"):
-            correctAnswer = loopQuestions[randomArrayChoice].answer;
-            break;
-        case ("dataTypes") :
-            correctAnswer = dataTypeQuestions[randomArrayChoice].answer;
-            break;
-        case ("arrays") :
-            correctAnswer = arrayQuestions[randomArrayChoice].answer;
-            break;
-        case ("comparisons") :
-            correctAnswer = comparisonQuestions[randomArrayChoice].answer;
-            break;
-        default:
-            console.log("something went wrong in final.js/gradeAnswer()");
         }
+    },
 
-    if (userQuizAnswer == (correctAnswer)) {  // if ... else
-        validated = true;
-        respondToAnswer(validated, correctAnswer); 
-    }
-    else {
-        console.log("false");
-        respondToAnswer(validated, correctAnswer);
-    }
-}
+    determineWin : function() {
+        console.log("determineWin ran");
+        Dealer.clearTheBoard();
 
-`
-Output to user if the answer was right or wrong. Calls addPoints()
-`
+        var titleElement = document.getElementById("tryAgainDiv").value
 
-function respondToAnswer(validated, correctAnswer) {
-    let answerResult = document.getElementById("answerResult")
-    document.getElementById("answerSocket").className = "deactivatedDiv";
+        
 
-    if (validated) {
-        answerResult.textContent = "Correct!"
-        addPoints();
-    }
-    else {
-        answerResult.textContent = `Sorry, incorrect. We were looking for: ${correctAnswer}`;
-    }
+        if (userPoints > dealerHand && userPoints < 22) {
+            document.getElementById("title").textContent = `Your points: ${userPoints}. Dealer: ${dealerHand}...You win!`  
+        }
+        else if (userPoints > 21) {
+            document.getElementById("title").textContent = `Your points: ${userPoints}. You've gone over 21. You lose...`
+        }
+        else if (userPoints < dealerHand){
+            document.getElementById("title").textContent = `Your points: ${userPoints} Dealer: ${dealerHand}...Dealer wins!`
+        }
+        
+
+        if (titleElement) {
+            title.append(document.getElementById("tryAgainDiv"))
+        }
+    },
+
+    askQuestion : function() {
+        console.log("askQuestion() ran")
     
-}
+        // document.getElementById("something2").value
+        var topicChoice = document.getElementById("categorySelector").value; 
+        currentTopic = topicChoice;
+        
+        
+        // clear the old responses
+        document.getElementById("answerResult").textContent = "";
+        document.getElementById("userAnswer").value = "";
 
+        randomArrayChoice = Math.floor(Math.random() * 3);
+        
+        // activate the answer template and append to the socket
+        document.getElementById("answerTemplate").className = "activatedDiv";
+        answerSocket.append(document.getElementById('answerTemplate'));
+        
 
-function addPoints() {
-    let userPointsToAdd  = Math.floor((Math.random() * 11) + 1);
+        switch (topicChoice){  // switch
 
-    answerResult.insertAdjacentText("beforeend", ` You received a card worth ${userPointsToAdd} points.`)
-    userPoints = userPoints + userPointsToAdd;
+            case "loops":
+                loopQuizzer.askQuestion(randomArrayChoice);
+                break;
+            case "dataTypes":
+                dataTypeQuizzer.askQuestion(randomArrayChoice);
+                break;
+            case "arrays":
+                arrayQuizzer.askQuestion(randomArrayChoice); 
+                break;
+            case "comparisons":
+                comparisonsQuizzer.askQuestion(randomArrayChoice);
+                break;
+            default:
+                console.log("Something went wrong in final.js/askQuestion()");
+        }
+    }, 
 
-    document.getElementById("pointCounter").textContent = `${name}, your current card count is ${userPoints}`;
+    gradeAnswer : function() {
+        console.log("gradeAnswer ran");
+        var validated = false;
+        document.getElementById("answerResult").className = "activatedDiv";
+        var correctAnswer;
+        var userQuizAnswer = document.getElementById("userAnswer").value;
+    
+        switch (currentTopic){
+            case ("loops"):
+                correctAnswer = loopQuestions[randomArrayChoice].answer;
+                break;
+            case ("dataTypes") :
+                correctAnswer = dataTypeQuestions[randomArrayChoice].answer;
+                break;
+            case ("arrays") :
+                correctAnswer = arrayQuestions[randomArrayChoice].answer;
+                break;
+            case ("comparisons") :
+                correctAnswer = comparisonQuestions[randomArrayChoice].answer;
+                break;
+            default:
+                console.log("something went wrong in final.js/gradeAnswer()");
+            }
+    
+        if (userQuizAnswer == (correctAnswer)) {  // if ... else
+            validated = true;
+            Dealer.respondToAnswer(validated, correctAnswer); 
+        }
+        else {
+            console.log("false");
+            Dealer.respondToAnswer(validated, correctAnswer);
+        }
+    },
 
-    if (userPoints > 21){
-        gameRunning = false;
-        determineWin();
+    respondToAnswer : function(validated, correctAnswer) {
+        var answerResult = document.getElementById("answerResult")
+        document.getElementById("answerSocket").className = "deactivatedDiv";
+
+        if (validated) {
+            answerResult.textContent = "Correct!"
+            Dealer.addPoints();
+        }
+        else {
+            answerResult.textContent = `Sorry, incorrect. We were looking for: ${correctAnswer}`;
+        }
+    },
+
+    addPoints : function() {
+        var userPointsToAdd  = Math.floor((Math.random() * 11) + 1);
+
+        answerResult.insertAdjacentText("beforeend", ` You received a card worth ${userPointsToAdd} points.`)
+        userPoints = userPoints + userPointsToAdd;
+
+        document.getElementById("pointCounter").textContent = `${name}, your current card count is ${userPoints}`;
+
+        if (userPoints > 21){
+            gameRunning = false;
+            Dealer.determineWin();
+        }
+    },
+
+    clearTheBoard : function() {
+        document.getElementById("categorySelector").className = "deactivatedDiv";
+        document.getElementById("questionSocket").className = "deactivatedDiv";
+        document.getElementById("answerResult").className = "deactivatedDiv";
+        document.getElementById("quizzer").className = "deactivatedDiv";
+        document.getElementById("pointCounter").className = "deactivatedDiv";
     }
-
 }
+
 
 
 // =============================Loop Objects and related Functions=============================
 
 
-`
-Loops question set
-`
 
 const loopQ1 = {
     question: "If you are trying to end a loop immediately, what keyword would you use?",
@@ -311,6 +285,10 @@ const arrayQuizzer = {
 
 // =============================Comparisons and Related Functions=============================
 
+
+// PLEASE NOTE: These are currently hard corded to get the module mock up for the final exam progress. It is intended that the user will at some level enter these numbers in themselves. This will not be submitted hard coded. 
+
+
 const comparisonsQ1 = {
     question: "What will [(6 < 7) && (10 > 5) || (2 == '2')] return?", // >, <, &&, ||, == 
     answer: "true",
@@ -335,6 +313,19 @@ const comparisonsQuizzer = {
         document.getElementById("answerSocket").className = "activatedDiv"
     }
 }
+
+// =============================onclick Assignments=============================
+// onclick assignments
+
+// FOR FINAL PROJECT PROGRESS: I'm having a very hard time with addEventListener, fell back to onclick
+
+document.getElementById("userAgreeToStart").onclick = Dealer.loadGameBoard;
+document.getElementById("askQuestion").onclick = Dealer.askQuestion;
+document.getElementById('userQuizAnswer').onclick = Dealer.gradeAnswer;
+document.getElementById('endGame').onclick = Dealer.determineWin;
+document.getElementById("tryAgain").onclick = Dealer.loadGameBoard;
+
+
 
 
 // =============================Items Remaining to Code=============================
