a55155d5ebff460173b390873ac21c4c0e50245c
Robert Daniels <robertdanielsvc@gmail.com>
2022-05-10 16:46:32 -0500

    commit misc notes

--- src/Project2/Contact.java
+++ src/Project2/Contact.java
@@ -8,21 +8,21 @@ package Project2;
  * AS ADAPTED FROM SEED FILE provided by Perla Weaver, Johnson County Community College.
  * 
  */
-public class Contact implements Comparable<Contact> {
+class Contact implements Comparable<Contact> {
 
 
 	private String firstName;
 	private String lastName;
 	private String phone;
 
-	public Contact(String firstName, String lastName, String phone) {
+	Contact(String firstName, String lastName, String phone) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.phone = phone;
 
 	}
 	
-	public Contact(String firstName, String lastName) {
+	Contact(String firstName, String lastName) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.phone = "";
